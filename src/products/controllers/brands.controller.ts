9498dc46791982fbc0fa27fc4b2950b4e962e709
JFernando12 <fire.jf2012@gmail.com>
2022-07-14 16:42:16 -0500

    Cambio

--- src/products/controllers/brands.controller.ts
+++ src/products/controllers/brands.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { BrandsService } from '../services/brands.service';
 
 @Controller('brands')
-export class BrandsController {}
+export class BrandsController {
+    constructor(private readonly brandsService: BrandsService) {}
+
+    @Get()
+    getBrands() {
+        return "All brands";
+    }
+
+    @Get(":id")
+    getBrand(@Param("id", ParseIntPipe) brandId: number) {
+        return `Brand ${brandId}`;
+    }
+
+    @Post()
+    createBrand(@Body() body: any) {
+        return `${body}`;
+    }
+
+    @Put(":id")
+    updateBrand(@Param("id", ParseIntPipe) brandId: number, @Body() body: any) {
+        return {
+            brandId,
+            body
+        }
+    }
+
+    @Delete(":id")
+    deleteBrand(@Param("id", ParseIntPipe) brandId: number) {
+        return `Brand ${brandId} updated`
+    }
+}
--- src/products/services/brands.service.ts
+++ src/products/services/brands.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BrandsService {}
+export class BrandsService {
+    private brandId = 1;
+    private brands = [
+        {
+            id: 1,
+            name: "Brand 1",
+            iamge: "https://algo.png"
+        }
+    ]
+
+    newID() {
+        this.brandId = this.brandId + 1;
+    }
+
+    getAll() {
+        return this.brands;
+    }
+
+    getOne(id: number) {
+        return this.brands.find(brand => brand.id === id);
+    };
+
+    create(data: any) {
+        this.newID();
+        const brand = {
+            id: this.brandId,
+            ...data
+        };
+
+        return brand;
+    };
+
+    update(id: number, data: any) {
+        const indexBrand = this.brands.findIndex(brand => brand.id === id);
+        
+    };
+
+    delete(id: number) {
+        this.brands = this.brands.filter(brand => brand.id !== id);
+        return this.brands;
+    }
+
+}
