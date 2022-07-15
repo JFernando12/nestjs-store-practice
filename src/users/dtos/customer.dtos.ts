3de52a49eef01b13036c60e62ffcc8a0b741d954
JFernando12 <fire.jf2012@gmail.com>
2022-07-15 14:53:32 -0500

    ss

--- src/users/dtos/customer.dtos.ts
+++ src/users/dtos/customer.dtos.ts
@@ -1,5 +1,5 @@
 import { PartialType } from "@nestjs/mapped-types";
-import { IsNotEmpty, IsPhoneNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateCustomerDto {
 
